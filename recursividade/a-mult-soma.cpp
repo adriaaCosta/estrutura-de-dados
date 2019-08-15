54c9b521df4fa36f29cd096676d79a61d86c8c81
Aluno <adria>
2019-08-14 21:46:09 -0400

    ajustes de retorno na tela

--- recursividade/a-mult-soma.cpp
+++ recursividade/a-mult-soma.cpp
@@ -7,12 +7,11 @@ float soma_mult(float n1, float n2){
 		return n1;	
 	}else if(n2==0){
 		return 0;	
-	}
-	else{
+	}else{
 		return n1 + soma_mult(n1, n2-1);
 	}
 }	
 main(){
 	float a1=3, a2=2;
-	printf("  %.2f", soma_mult(a1, a2));
+	printf("Resultado: %.2f", soma_mult(a1, a2));
 }
